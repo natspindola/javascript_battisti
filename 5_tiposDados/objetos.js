d6e53c4a2b52f0f8892b5e89a1b779edadbde121
natspindola <natspindola@hotmail.com>
2022-03-08 19:26:11 -0300

    dados: objetos

---
+++ 5_tiposDados/objetos.js
@@ -0,0 +1,18 @@
+var obj = {
+    nome: "Natalia",
+    idade: 25,
+    profissao: "Programadora",
+    estaTrabalhando: false,
+};
+
+console.log(obj);
+console.log(typeof obj);
+
+console.log(obj.nome);
+console.log(obj.idade);
+console.log(obj.profissao);
+
+console.log("O meu nome é " + obj.nome);
+
+obj.graduacao = true;
+console.log(obj);
