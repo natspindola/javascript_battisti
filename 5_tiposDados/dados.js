6df59213df40c610df0d2bc02e6ca2bb09f6b237
natspindola <natspindola@hotmail.com>
2022-03-08 18:48:29 -0300

    dados: number

---
+++ 5_tiposDados/dados.js
@@ -0,0 +1,16 @@
+var numero = 5;
+console.log(numero);
+console.log(typeof numero);
+
+var float = 5.32;
+console.log(float);
+console.log(typeof float);
+
+var numeroNoTexto = '523';
+console.log(numeroNoTexto);
+console.log(typeof numeroNoTexto); //string
+
+console.log(NaN);
+console.log(typeof NaN);
+console.log(typeof +Infinity);
+console.log(typeof -Infinity);
