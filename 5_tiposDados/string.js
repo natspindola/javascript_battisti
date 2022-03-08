ffae33d8430e5e659ff2ecd824177de18fde457b
natspindola <natspindola@hotmail.com>
2022-03-08 18:59:23 -0300

    dados: string

---
+++ 5_tiposDados/string.js
@@ -0,0 +1,15 @@
+var nome = "Natalia";
+var sobrenome = "Spindola";
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(nomeCompleto);
+console.log(typeof nomeCompleto);
+
+console.log(typeof "5.54451");
+
+var frase = 'Essa é uma frase complexa';
+console.log(frase);
+console.log(typeof frase);
