96e18901147fd631e81c250c2dbb196d11593709
natspindola <natspindola@hotmail.com>
2022-03-08 18:28:14 -0300

    particularidades

---
+++ 3_particularidades/particularidades.js
@@ -0,0 +1,9 @@
+//o JS executa o código por linhas, começando pra primeira até a última
+console.log("Primeiro");
+console.log("último");
+console.log("mais um console.log");
+
+// variável com conteúdo de número muda facilmente para texto, a linguagem é tipamente fraca
+var a = 1;
+a = 'teste';
+console.log(a);
