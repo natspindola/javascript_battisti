75ccc9c9c62cf4ca1b035aa7830c8146bc4ce3e7
natspindola <natspindola@hotmail.com>
2022-03-08 18:38:29 -0300

    variáveis

---
+++ 4_variaveis/variaveis.js
@@ -0,0 +1,16 @@
+var teste = 1;
+
+teste = "1";
+console.log(teste);
+
+teste = "Natalia";
+console.log(teste);
+
+var nome = "João";
+console.log(nome);
+
+//os unicos caracteres especiais aceitos
+var $nome = "teste1";
+var _nome = "teste2";
+console.log($nome);
+console.log(_nome);
