2942202399a8d3ca000990e9d8cc103e96354946
natspindola <natspindola@hotmail.com>
2022-03-08 19:38:21 -0300

    dados: array

---
+++ 5_tiposDados/arrays.js
@@ -0,0 +1,10 @@
+var arr = [5, "Natalia", true, { teste: 1, teste: 2 }]; //obj => {}
+console.log(arr);
+
+var arr2 = [2, 3, 4, 5, 6];
+console.log(arr2);
+
+console.log(arr[1]);
+console.log(arr2[0]);
+
+console.log(typeof arr);
