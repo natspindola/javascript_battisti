829f9dbb432848f1d4a18a0c81978c938e3cb23f
natspindola <natspindola@hotmail.com>
2022-03-09 18:57:46 -0300

    or

---
+++ 11_operador_or/or.js
@@ -0,0 +1,14 @@
+var idade = 16;
+var nome = "João";
+
+if(nome == "João" || idade > 14) {
+    console.log("Pode entrar na aula de esgrima");
+} else {
+    console.log("Não pode entrar na aula");
+}
+
+if(nome == "João" && 15 > 20 || 10 == 10) {
+    console.log("Teste");
+} else {
+    console.log("Não entrou");
+}
