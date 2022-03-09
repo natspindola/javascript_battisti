7e29da7b3fb75aecebb72e7a03ba3520704d1118
natspindola <natspindola@hotmail.com>
2022-03-09 17:38:41 -0300

    comparacao

---
+++ 7_operadoresComparacao/comparacao.js
@@ -0,0 +1,28 @@
+var idade = 18;
+var possuiCarro = 0;
+
+if (idade >= 18) {
+    console.log("O usuário pode tirar a habilitação.");
+}
+
+if (idade <= 17) {
+    console.log("O usuário não pode tirar a habilitação.")
+}
+
+if (possuiCarro) {
+    console.log("O usuário pode dirigir o carro.")
+}
+
+var nome = "Matheus";
+
+if (nome == "Matheus") {
+    console.log("o seu nome é Matheus");
+}
+
+if (nome != "Matheus") {
+    console.log("o seu nome não é Matheus");
+}
+
+if (100 < 20) {
+    console.log("Passou 2")
+}
