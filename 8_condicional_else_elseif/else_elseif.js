397b4ffbf82a4c601a7a5392a06be7a801fdb2f7
natspindola <natspindola@hotmail.com>
2022-03-09 17:48:08 -0300

    else_elseif

---
+++ 8_condicional_else_elseif/else_elseif.js
@@ -0,0 +1,19 @@
+var nome = "Henrique";
+
+if(nome == "Pedro") {
+    console.log("O nome dele é Pedro");
+} else if(nome == "Matheus") {
+    console.log("O nome é Matheus");
+} else if(nome == "Henrique") {
+        console.log("O nome é Matheus");
+} else {
+    console.log("Ele possui outro nome");
+}
+
+var idade = 19;
+
+if(idade > 20){
+    console.log("Ele pode entrar na festa!");
+} else if(idade >= 18) {
+    console.log("Ele só pode entrar com autorização");
+}
