a356f1726143d6ec8a94500c11a6a5e0442a695e
natspindola <natspindola@hotmail.com>
2022-03-10 20:06:07 -0300

    funções

---
+++ 17_funcoes/funcoes.js
@@ -0,0 +1,29 @@
+function primeiraFuncao() {
+    console.log("Hello word das funçoes")
+}
+
+primeiraFuncao();
+
+function dizerNome(nome) {
+    console.log("O nome é: " + nome);
+}
+
+dizerNome("Natalia");
+dizerNome("Pedro");
+
+var nomeDoBancoDeDados = "João";
+
+dizerNome(nomeDoBancoDeDados);
+
+function soma(a, b) {
+    var soma = a + b;
+    return soma;
+}
+
+var somaUm = sina(2, 5);
+console.log(somaUm);
+
+var somaDois = soma(5, 5);
+console.log(somaDois);
+
+console.log(soma(4, 5));
