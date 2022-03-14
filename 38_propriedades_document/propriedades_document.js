7403913667d8565b2d9e39a9fc0956f33a4d8150
natspindola <natspindola@hotmail.com>
2022-03-14 19:31:27 -0300

    propriedades document

---
+++ 38_propriedades_document/propriedades_document.js
@@ -0,0 +1,16 @@
+// propriedades document
+console.log(document.body); //imprime o body
+
+console.log(document.head); //imprime o head
+
+console.log(document.links[0]); //imprime os links
+
+document.links[0].textContent = "Twitter"; //altera o nome de um link
+
+console.log(document.URL); //imprime a URL da página ou caminho do arquivo
+
+console.log(document.title); //imprime o title da página
+
+document.title = "Aula 38"; //altera o title
+
+console.log(document.title);
