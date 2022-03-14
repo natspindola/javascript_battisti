c6d9d76c02dd4ace20ffe64dfa5668bfa653e757
natspindola <natspindola@hotmail.com>
2022-03-14 19:07:51 -0300

    adicionando CSS pelo DOM

---
+++ 37_alterando_CSS/alterando_css.js
@@ -0,0 +1,16 @@
+// seleciona o elemento
+
+var title = document.querySelector("#title");
+
+// adiciona o estilo
+
+title.style.color = "red";
+
+// background-color
+title.style.backgroundColor = "yellow";
+
+//seleciona o elemento
+var subtitle = document.querySelector(".subtitle");
+
+// adicionar vários estilos
+subtitle.style.cssText = "color: blue; background-color: pink; font-size: 50px";
