998d585d8afb217c69f47f9e56e84eac5952042a
natspindola <natspindola@hotmail.com>
2022-03-14 18:09:48 -0300

    alterar conteúdo do elemento

---
+++ 31_alterando_conteudo/alterando_conteudo.js
@@ -0,0 +1,12 @@
+// selecionar o elemento
+var title = document.querySelector("#title");
+
+// innerHTML
+title.innerHTML = "Testando o innerHTML"
+
+// textContent -> mais utilizado
+var subtitle = document.querySelector('.subtitle');
+
+console.log(subtitle);
+
+subtitle.textContent = "Testando o textContent"
