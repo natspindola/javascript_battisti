f6a41e927373505c1a52e3bfc86b49a75cae1bb6
natspindola <natspindola@hotmail.com>
2022-03-15 18:34:07 -0300

    evento click

---
+++ 43_evento_click/evento_click.js
@@ -0,0 +1,31 @@
+var btn = document.querySelector("#btn");
+
+console.log(btn);
+
+btn.addEventListener("click", function() {
+   
+    console.log("clicou");
+   
+    console.log(this);
+   
+    this.style.color = "red";
+});
+
+// click afetando outros elementos
+
+var title = document.querySelector("#title")/
+
+title.addEventListener("click", function() {
+
+    var subtitle = document.querySelector(".subtitle");
+
+    subtitle.style.display = "none";
+});
+
+// double click
+
+var subtitle = document.querySelector(".subtitle");
+
+subtitle.addEventListener("dbclick", function() {
+    console.log("click duplo");
+});
