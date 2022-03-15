6c2385a84bb717ae54d138444b4c99a7b6627d79
natspindola <natspindola@hotmail.com>
2022-03-15 18:52:39 -0300

    mouseover e mouseout

---
+++ 44_evento_,mouseover_mouseout/mouseover_mouseout.js
@@ -0,0 +1,32 @@
+// evento mouseover
+
+var title = document.querySelector("#title");
+
+title.addEventListener("mouseover", function() {
+    this.style.backgroundColor = "yellow";
+});
+
+
+// evento mouseout
+title.addEventListener("mouseout", function() {
+    this.style.backgroundColor = "white";
+});
+
+// afetar outro elemento com mouseover
+var subtitle = document.querySelector("#subtitle");
+
+subtitle.addEventListener("mouseover", function() {
+
+    var legenda = document.querySelector("#legenda");
+
+    legenda.classList.remove("hide");
+})
+
+var subtitle = document.querySelector("#subtitle");
+
+subtitle.addEventListener("mouseout", function() {
+
+    var legenda = document.querySelector("#legenda");
+
+    legenda.classList.remove("hide");
+})
